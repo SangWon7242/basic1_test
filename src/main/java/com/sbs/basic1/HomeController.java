9c07645628cc556760c50751c474d4e00c619841
SangWon7242 <gotkdqja@gmail.com>
2024-02-23 01:31:03 +0900

    C:/Program Files/Git/home/main2, /home/main3

--- src/main/java/com/sbs/basic1/HomeController.java
+++ src/main/java/com/sbs/basic1/HomeController.java
@@ -2,6 +2,7 @@ package com.sbs.basic1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 // @Controller 의 의미
 // 개발자가 스프링부트에게 말한다.
@@ -15,7 +16,20 @@ public class HomeController {
   @GetMapping("/home/main")
   // @ResponseBody 의 의미
   // 아래 메서드를 실행한 후 그 리턴값을 응답으로 삼아줘
+  @ResponseBody
   public String showHome() {
     return "안녕하세요.";
+  }
+
+  @GetMapping("/home/main2")
+  @ResponseBody
+  public String showHome2() {
+    return "환영합니다.";
+  }
+
+  @GetMapping("/home/main3")
+  @ResponseBody
+  public String showHome3() {
+    return "반갑습니다.";
   }
 }
