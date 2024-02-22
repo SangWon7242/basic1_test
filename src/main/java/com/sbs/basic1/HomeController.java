840e44ec119e204ab0e1120e2d3b0ade255b104e
SangWon7242 <gotkdqja@gmail.com>
2024-02-23 02:00:57 +0900

    파라미터가 정상 작동하도록 옵션 세팅

--- src/main/java/com/sbs/basic1/HomeController.java
+++ src/main/java/com/sbs/basic1/HomeController.java
@@ -52,7 +52,7 @@ public class HomeController {
   // 개발자가 스프링부트에게 말한다.
   // int a 는 쿼리스트링에서 a 파라미터의 값을 얻은 후 정수화 한 값이어야 한다.
   // @RequestParam 는 생략가능
-  public int showPlus(@RequestParam(value = "a", defaultValue = "0") int a, @RequestParam(value = "b") int b) {
+  public int showPlus(@RequestParam(defaultValue = "0") int a, @RequestParam int b) {
     return a + b;
   }
 }
