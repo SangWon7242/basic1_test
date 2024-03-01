22088f16e2a7fbe2825cf6f4d4c93dbab7827ceb
SangWon7242 <gotkdqja@gmail.com>
2024-03-01 21:53:00 +0900

    사람 수정기능

--- src/main/java/com/sbs/basic1/HomeController.java
+++ src/main/java/com/sbs/basic1/HomeController.java
@@ -239,6 +239,25 @@ public class HomeController {
 
     return "%d번 사람이 삭제되었습니다.".formatted(id);
   }
+
+  @GetMapping("/home/modifyPerson")
+  @ResponseBody
+  public String modifyPerson(int id, String name, int age) {
+    Person found = people
+        .stream()
+        .filter(p -> p.getId() == id) // 해당 녀석이 참인것만 필터링
+        .findFirst()// 필터링 결과 하나만 남는데, 그 하나 남은 걸 가져오겠다.
+        .orElse(null); // 없으면 null을 넣어라
+
+    if (found == null) {
+      return "%d번 사람이 존재하지 않습니다.".formatted(id);
+    }
+
+    found.setName(name);
+    found.setAge(age);
+
+    return "%d번 사람이 수정되었습니다.".formatted(id);
+  }
 }
 
 class Car {
@@ -287,9 +306,10 @@ class CarV2 {
 class Person {
   private static int lastId;
   private final int id;
-  private final String name;
-  private final int age;
-
+  @Setter
+  private String name;
+  @Setter
+  private int age;
 
   static {
     lastId = 0;
